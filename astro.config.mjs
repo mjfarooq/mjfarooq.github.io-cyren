7bed735c75e4f00c87c42246b982882502b7a2ab
Muhammad Junaid Farooq <junaidbmi@gmail.com>
2026-05-23 02:48:53 +0200

    CyRen base update

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://mjfarooq.github.io',
+  base: '/mjfarooq.github.io-cyren',
+  build: { format: 'directory' },
+});
